0a75e7f380706ed3e092e8bf7c86ad0e8a4b722b
Polina Avdeeva <polinaavdeeva55@gmail.com>
2023-08-20 04:53:37 +0300

    fix: try regex for cards

--- middlewares/cardValidation.js
+++ middlewares/cardValidation.js
@@ -1,15 +1,15 @@
 const { celebrate, Joi } = require('celebrate');
-const regExp = require('../utils/constants');
+const { regExp } = require('../utils/constants');
 
 module.exports.validateCard = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required().pattern(regExp),
-  }),
+    body: Joi.object().keys({
+        name: Joi.string().min(2).max(30).required(),
+        link: Joi.string().required().pattern(regExp),
+    }),
 });
 
 module.exports.validateCardId = celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().required().hex().length(24),
-  }),
+    params: Joi.object().keys({
+        cardId: Joi.string().required().hex().length(24),
+    }),
 });
